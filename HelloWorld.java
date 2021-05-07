40e8b6f56ca0b2d449d38f544d278dd1bc949199
Santoshgs13 <81846783+Santoshgs13@users.noreply.github.com>
2021-05-07 17:25:15 +0530

    3rd change

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("\n Hello, ");
+		System.out.println("\n Welcome to Treosoftit ");
 		System.out.println("\n Welcome to Waycool ");
 	}
 
